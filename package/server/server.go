b96c8c7a0dfeabd7e0c7e6022060b7da137058da
Rekciuq <danilaka76@outlook.com>
2025-07-26 00:44:24 +0300

    configured cors

--- package/server/server.go
+++ package/server/server.go
@@ -13,6 +13,21 @@ import (
 	uh "github.com/Rekciuq/go-bucket/package/userHandler"
 )
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func StartServer(ctx context.Context, client *ent.Client) *http.Server {
 	router := http.NewServeMux()
 	router.Handle("/v1/image/", http.StripPrefix("/v1/image", ih.ImageHandler(&entclient.ClientConnection{Ctx: ctx, Client: client})))
@@ -20,7 +35,7 @@ func StartServer(ctx context.Context, client *ent.Client) *http.Server {
 	router.Handle("/v1/upload/", http.StripPrefix("/v1/upload", uploadhandler.UploadHandler(&entclient.ClientConnection{Ctx: ctx, Client: client})))
 	server := http.Server{
 		Addr:    fmt.Sprintf(":%d", config.DEFAULT_PORT),
-		Handler: router,
+		Handler: corsMiddleware(router),
 	}
 
 	fmt.Printf("Server listening on port %d\n", config.DEFAULT_PORT)
